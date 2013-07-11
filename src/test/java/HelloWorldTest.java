51555f0c15a1c9cc41f2478e21e3aa4056f750b4
overriderb <overriderb@gmail.com>
2013-07-11 11:53:18 +0300

    First commit with structure and test files.

---
+++ src/test/java/HelloWorldTest.java
@@ -0,0 +1,13 @@
+import org.junit.Test;
+
+/**
+ * User: Viktor_Khvostov
+ * Date: 7/11/13
+ * Time: 11:49 AM
+ */
+public class HelloWorldTest {
+    @Test
+    public void testSayHello() throws Exception {
+
+    }
+}
