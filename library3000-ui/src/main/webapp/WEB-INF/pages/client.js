a4081713238c876dd37e37ab6ee60775d397142b
override <overriderb@gmail.com>
2014-01-05 01:15:18 +0200

    Added space invader and some changes in logo.

--- library3000-ui/src/main/webapp/WEB-INF/pages/client.js
+++ library3000-ui/src/main/webapp/WEB-INF/pages/client.js
@@ -1,7 +1,11 @@
+var isLogoOpened;
+
 $(document).ready(function(){
     $("#clickArea").click(function() {
 //                    $("#logo_img").fadeIn(1000);
         $("#logo").slideToggle("slow");
+
+        changeClickText();
     });
 });
 
@@ -12,4 +16,17 @@ function showMyName(name) {
 function showHello() {
     var name = prompt("Type your name ", "");
     showMyName(name);
+}
+
+function changeClickText() {
+    if(isLogoOpened == null) {
+        $("#clickArea").text("Close Logo!");
+        isLogoOpened = true;
+    } else if(isLogoOpened === true) {
+        $("#clickArea").text("Open Logo!");
+        isLogoOpened = false;
+    } else {
+        $("#clickArea").text("Close Logo!");
+        isLogoOpened = true;
+    }
 }
