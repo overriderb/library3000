138d8617db8eba7513ba1aac0541f5a8b1bcca24
override <overriderb@gmail.com>
2014-01-04 18:05:31 +0200

    Added flying library logo.

--- library3000-ui/src/main/webapp/WEB-INF/pages/client.js
+++ library3000-ui/src/main/webapp/WEB-INF/pages/client.js
@@ -1,3 +1,10 @@
+$(document).ready(function(){
+    $("#clickArea").click(function() {
+//                    $("#logo_img").fadeIn(1000);
+        $("#logo").slideToggle("slow");
+    });
+});
+
 function showMyName(name) {
     alert("Hello " + name);
 }
